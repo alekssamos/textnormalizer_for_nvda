45fb55dd0bdf785e976a77a818a6cae7b4d5bd71
alkssamos <aleks-samos@yandex.ru>
2025-06-17 06:58:55 +0300

    fix version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Дополнение Text Normalizer для NVDA
 
-[Скачать версию 2025.06.16](https://github.com/alekssamos/textnormalizer_for_nvda/releases/latest/download/textnormalizer-2025.06.16.nvda-addon)
+[Скачать версию 2025.06.17](https://github.com/alekssamos/textnormalizer_for_nvda/releases/latest/download/textnormalizer-2025.06.16.nvda-addon)
 
 ### Горячие клавиши
 Свободных комбинаций нынче стало не хватать, да и они могут уже использоваться в других дополнениях.
--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Translates the letters of the alphabet mixed in normal."""),
 	# version
-	"addon_version": "2025.06.16",
+	"addon_version": "2025.06.17",
 	# Author(s)
 	"addon_author": u"alekssamos <aleks-samos@yandex.ru>",
 	# URL for the add-on documentation support
@@ -27,7 +27,7 @@ addon_info = {
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": "2019.3.1",
+	"addon_minimumNVDAVersion": "2019.1.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
 	"addon_lastTestedNVDAVersion": "2025.1.0",
 	# Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
